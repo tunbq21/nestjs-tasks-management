2533b0592d7bd0fd92c3298f0c3512c26940d20f
MokaMoka77 <tbuiquang103@gmail.com>
2025-09-01 17:37:36 +0700

    Update  project

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import type { Task } from './tasks.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { get } from 'http';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }   
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task | undefined {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Post()
+    // createTask(@Body('title') title: string, @Body('description') description: string,): Task {
+    //     return this.tasksService.createTask(title, description);
+    // }
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.tasksService.createTask(createTaskDto);
+    }
+}
