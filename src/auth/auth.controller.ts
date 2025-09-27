a06c37151cd09ea405e53e83de306658fc723037
MokaMoka77 <tbuiquang103@gmail.com>
2025-09-28 00:49:06 +0700

    create get-user.decorator.ts file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -15,4 +16,10 @@ export class AuthController {
     signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
         return this.authService.signIn(authCredentialsDto);
     }
+
+    // @Post('/test')
+    // @UseGuards(AuthGuard())
+    // test(@Req() req) {
+    //     console.log(req);
+    // }
 }
